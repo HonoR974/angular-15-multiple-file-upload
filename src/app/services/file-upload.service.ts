b38cbaf5e631e86d26890b717ecbefd63888db9b
honoR974 <honore.guillaudeau1@gmail.com>
2023-04-02 14:41:50 +0200

    modif

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FileUploadService {
-  private baseUrl = 'http://localhost:8080';
+  private baseUrl = 'http://localhost:8081/api/file';
 
   constructor(private http: HttpClient) { }
 
